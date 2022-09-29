81d855821d5b5dd1a16cf5c7abaa4d4a59fdd52e
lucagaddini <lucagaddini@icloud.com>
2022-09-29 16:24:04 +0200

    Revert "Inizializzazione Repo"
    This reverts commit eb3fc03e4930c3c5b7db7525727c0ff66c26be82.

--- DVD.cs
+++
@@ -1,9 +0,0 @@
-﻿public class DVD : Prodotto
-{
-    public int Durata { get; set; }
-
-    public DVD(int durata, string codice, string titolo, int anno, string settore, bool èDisponibile, int scaffale, string autore) : base(codice, titolo, anno, settore, èDisponibile, scaffale, autore)
-        {
-            this.Durata = durata;
-        }
-}
