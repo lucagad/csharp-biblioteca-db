98974bca3a48f146d5bd9bbd79fae3107fa5cd01
lucagaddini <lucagaddini@icloud.com>
2022-09-29 16:57:43 +0200

    Cancellate classi - Aggiunta Connessione DB - Aggiunto file Query

--- Program.cs
+++ Program.cs
@@ -1,55 +1,46 @@
-//Esercizio:
-//Si vuole progettare un sistema per la gestione di una biblioteca. Gli utenti si possono registrare al sistema, fornendo:
-//    cognome,
-//    nome,
-//    email,
-//    password,
-//    recapito telefonico,
 
-//Gli utenti registrati possono effettuare dei prestiti sui documenti che sono di vario tipo (libri, DVD). I documenti sono caratterizzati da:
-//    un codice identificativo di tipo stringa (ISBN per i libri, numero seriale per i DVD),
-//    titolo,
-//    anno,
-//    settore(storia, matematica, economia, …),
-//    stato(In Prestito, Disponibile),
-//    uno scaffale in cui è posizionato,
-//    un autore (Nome, Cognome).
 
-//Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
+using System.Data;
+using System.Data.SqlClient;
+string stringaDiConnessione = "Data Source=localhost;Initial Catalog=db_biblioteca;Persist Security Info=True;User ID=sa;Password=NET2022!";
+SqlConnection connessioneSq1 = new SqlConnection(stringaDiConnessione);
 
-//L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
-//Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+try
+{
+    connessioneSq1.Open();
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+} 
+finally {
+    connessioneSq1.Close();
+}
 
-using System.Runtime.ConstrainedExecution;
+using (SqlConnection connessioneSql = new SqlConnection(stringaDiConnessione))
+{
+    try
+    {
+        connessioneSql.Open();
+        // dichiaro la query da eseguire
+        string query = "SELECT *  FROM categories";
+        // creo il comando ed eseguo la query
+        using (SqlCommand cmd = new SqlCommand(query, connessioneSql))
+        using (SqlDataReader reader = cmd.ExecuteReader())
+            while (reader.Read())
+            {
+                ReadSingleRow((IDataRecord)reader);
+            }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+}
 
-// Lista di utenti registrati
-List<Utente> listaIscritti = new List <Utente>();
-
-// Lista di libri
-List <Libro> listaLibri = new List <Libro>();
-
-// Lista di DVD
-List <DVD> listaDVD = new List <DVD>();
-
-// Creazione di utenti di test
-Utente utente1 = new Utente("Rossi", "Mario","rossi.mario@gmail.com","Password", "0583142536" );
-Utente utente2 = new Utente("Rossi", "Paolo", "Rossi.Paolo@gmail.com", "Password", "0583142536");
-Utente utente3 = new Utente("Bianchi", "Mario", "Bianchi.Mario@gmail.com", "Password", "0583142536");
-listaIscritti.Add(utente1);
-listaIscritti.Add(utente2);
-listaIscritti.Add(utente3);
-
-// Creazione di libri di test
-Libro libro1 = new Libro("1000001", "Titolo Libro 1", 2020, "Fantasy", true, 1,"Autore 1",100);
-Libro libro2 = new Libro("1000002", "Titolo Libro 2", 2020, "Thriller", false, 2, "Autore 2", 200);
-listaLibri.Add(libro1);
-listaLibri.Add(libro2);
-
-// Creazione di DVD di test
-DVD DVD1 = new DVD(120, "2000001", "Titolo DVD 1", 2020, "Fantasy", true, 1, "Regista 1");
-DVD DVD2 = new DVD(180, "2000002", "Titolo DVD 2", 2020, "Thriller", false, 2, "Regista 2");
-listaDVD.Add(DVD1);
-
+static void ReadSingleRow(IDataRecord dataRecord)
+{
+    Console.WriteLine(String.Format("{0} - {1}", dataRecord[0], dataRecord[1]));
+}
 
 int scelta = MenuIniziale();
 
@@ -110,10 +101,7 @@ switch (scelta)
 
                         // 1 NOME / 2 CODICE
                         int sceltaTipoRicercaDVD = MenuRicercaProdotto();
-
                         
-
-
                         break;
 
                     // LIBRO
@@ -132,9 +120,7 @@ switch (scelta)
 
                             CercaLibro(sceltaTipoRicercaLibro, Convert.ToString( chiaveRicercaLibro));
                         }
-
-
-
+                        
                         break;
 
                     default:
@@ -324,7 +310,6 @@ int MenuRicercaProdotto()
     return sceltaTipoRicerca;
 
 }
-
 
 // Inseritmento Nome/Codice
 string InserimentoNomeCodice()
